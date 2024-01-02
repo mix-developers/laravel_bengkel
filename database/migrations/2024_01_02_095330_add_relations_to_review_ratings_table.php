cf4e7c5986e479584182903163e8576d045b91e4
firchun025 <firchun025@gmail.com>
2024-01-02 19:03:58 +0900

    add relationships on table

---
+++ database/migrations/2024_01_02_095330_add_relations_to_review_ratings_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('review_ratings', function (Blueprint $table) {
+            $table->foreign('id_user')->references('id')->on('users');
+            $table->foreign('id_service')->references('id')->on('services');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('review_ratings', function (Blueprint $table) {
+            //
+        });
+    }
+};
---
+++ database/migrations/2024_01_02_095538_add_relations_to_service_finisheds_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('service_finisheds', function (Blueprint $table) {
+            $table->foreign('id_service')->references('id')->on('services');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('service_finisheds', function (Blueprint $table) {
+            //
+        });
+    }
+};
